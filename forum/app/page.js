af6a2bef513b3e2999c3d7ad5660a78f855c828c
kongchip <myunghwan0321@gmail.com>
2023-06-30 01:47:33 +0900

    db데이터 가져오기

--- forum/app/page.js
+++ forum/app/page.js
@@ -1,11 +1,11 @@
 import { connectDB } from '@/util/database.js';
 
 export default async function Home() {
-  const client = await connectDB;
-  const db = client.db('forum');
-
+  // const client = await connectDB;
+  // const db = client.db('forum');
+  const db = (await connectDB).db('forum');
   let result = await db.collection('post').find().toArray();
-  console.log(result);
+
   return (
     <div>
       <div></div>
